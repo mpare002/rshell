248a8521131642ec89d5293ddc0547e267302a97
rulep001 <rulep001@ucr.edu>
2016-11-03 05:58:49 +0000

    Changed test files

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 ### this is our commented command test ###
 ../bin/rshell < commented_command
+../bin/rshell < echo hello #world
+../bin/rshell < ls -a || echo hello world #testing comments on multiple commands
+../bin/rshell < ps #exit
+../bin/rshell < echo test # || ls
+../bin/rshell < #
+../bin/rshell < mkdir test_directory #create a test directory
+../bin/rshell < rm -r test_directory #remove the directory
+../bin/rshell < echo test && echo test2 # echo both statements
+../bin/rshell < touch file.cpp #testing touch with a file name
+../bin/rshell < #rm file.cpp #remove the file
+../bin/rshell < #at file.cpp #testing cat on empty file
+../bin/rshell < echo mkdir test_dir # echo test
+../bin/rshell < ls -l #
+../bin/rshell < exit
--- tests/multi_command.sh
+++ tests/multi_command.sh
@@ -2,5 +2,20 @@
 
 #here is our multiple command test#
 
-
 ../bin/rshell < multi_command_test
+../bin/rshell < echo hello world || ls -a; ps
+../bin/rshell < ls -a && mkdir test_directory
+../bin/rshell < ls #rm -r test_directory;ls 
+../bin/rshell < echo hello #mkdir test_directory2 && echo made test directory
+../bin/rshell < #rm -r test_directory2; ls
+../bin/rshell < #echo testing clear; clear
+../bin/rshell < echo || ls -m && echo testing || mkdir test_dir
+../bin/rshell < ps && ls -r
+../bin/rshell < cat nonexistantfile || echo testing invalid cat
+../bin/rshell < ls -q; ps
+../bin/rshell < touch test.cpp; echo created test.cpp && rm test.cpp
+../bin/rshell < echo || mkdir || ls
+../bin/rshell < ls -R && ps
+../bin/rshell < ps; ps || ls && ps
+../bin/rshell < ls -a || ls && ls
+../bin/rshell < exit
