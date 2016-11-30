edc78f6a39b405ad848a35db7924f2d022bb274f
Michael Pare <mpare002@ucr.edu>
2016-11-30 18:49:42 -0500

    added directory to prompt

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -22,8 +23,10 @@ typedef mytok::iterator tok_it;
 
 int main()
 {
-	while(1) {
-		try {
+	while(1) 
+	{
+		try 
+		{
 			// Declare variables
 			char *login = getlogin();
 			char host[200];
@@ -31,12 +34,13 @@ int main()
 			string bash_input;
 			
 			// Output user information
-			cout << login << "@" << host << "$ ";
+			cout << login << "@" << host << ':' << getenv("PWD") << " $ ";
 			getline(cin, bash_input);
 	
 			// Look for comments and remove comments if necessary
 			size_t s = bash_input.find("#");
-			if (s != string::npos) {
+			if (s != string::npos) 
+			{
 				bash_input.erase(s);
 			}
 			
@@ -45,16 +49,20 @@ int main()
 			
          
          // Now look for exit as only command and quit if necessary
-			if (bash_command == "exit") {
+			if (bash_command == "exit") 
+			{
 				exit(0);
 			}
 			
 			// Create commands and execute bash line
 			Base* start = parse(bash_command);
-         if (start != 0) {
+        	if (start != 0) 
+        	{
 				start->execute();
 			}
+
 			delete start;
+
 		}
 		catch (runtime_error &e) {
 			cout << "-rshell: " << e.what() << endl;
